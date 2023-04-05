7b7ca993386de3c2cf31e11146e3a873e2ac3bbe
BRobersonO <BRobersonO@gmail.com>
2023-04-05 08:58:48 -0400

    more API learning

--- Day33APIs/main.py
+++ Day33APIs/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime as dt
 
 # response = requests.get(url="http://api.open-notify.org/iss-now.json")
 # response.raise_for_status()
@@ -19,8 +20,16 @@ MY_LONG = -77.279340
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
+    "formatted": 0,
 }
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-print(data)
+
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+time_now = dt.now().hour
+print(time_now)
